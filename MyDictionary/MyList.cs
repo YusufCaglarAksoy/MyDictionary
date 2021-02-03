27198b0d370cc9f3f25eb2de39a40044863fbe3f
Yusuf Çağlar Aksoy <yusufcaglar_aksoy@hotmail.com>
2021-02-03 20:22:02 +0300

    Add project files.

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyList
+{
+    class MyList<A, B>
+    {
+        A[] ProductName;
+        B[] ProductPrice;
+
+        A[] TempNames;
+        B[] TempPrices;
+
+        public MyList()
+        {
+            ProductName = new A[0];
+            ProductPrice = new B[0];
+
+        }
+
+        public void Add(A name, B price)
+        {
+            TempNames = ProductName;
+            TempPrices = ProductPrice;
+
+            ProductName = new A[ProductName.Length + 1];
+            ProductPrice = new B[ProductPrice.Length + 1];
+
+            for (int i = 0; i < TempNames.Length; i++)
+            {
+                ProductName[i] = TempNames[i];
+            }
+
+            ProductName[ProductName.Length - 1] = name;
+
+            for (int j = 0; j < TempPrices.Length; j++)
+            {
+                ProductPrice[j] = TempPrices[j];
+            }
+
+            ProductPrice[ProductPrice.Length - 1] = price;
+
+        }
+
+        public A[] Names
+        {
+            get { return ProductName; }
+        }
+
+        public B[] Prices
+        {
+            get { return ProductPrice; }
+        }
+
+
+    }
+}
+  
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList <string,int> myList = new MyList<string, int>();
+            myList.Add("Elma", 5);
+            myList.Add("Portakal",4);
+            myList.Add("Mandalina", 7);
+            myList.Add("Cilek", 20);
+            myList.Add("Erik", 15);
+            for (int i=0; i < 4; i++)
+            {
+                Console.WriteLine("Urun adi: " + myList.Names[i] + "   Urun Fiyati: " + myList.Prices[i] + "TL");
+            }
+        }
+    }
+}
